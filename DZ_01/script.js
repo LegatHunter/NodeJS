7d516b215be636e05640ffe256de0ba14931f442
Nikita Prebyshevskiy <legathunter@mail.ru>
2024-09-05 11:30:52 +0300

    DZ_01 done!

--- DZ_01/script.js
+++ DZ_01/script.js
@@ -11,16 +11,16 @@ let errorCount = 1;
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.end(`<a href = '/about'> Перейти на страницу обо мне<a/>
-      <p>Количество переходов = ${mainCount++}<p/>`);
+    res.end(`<a href = '/about'> Перейти на страницу обо мне</a>
+      <p>Количество переходов = ${mainCount++}</p>`);
   } else if (req.url === "/about") {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.end(`<a href = '/'> Перейти на страницу главная<a/>
-      <p>Количество переходов = ${aboutCount++}<p/>`);
+    res.end(`<a href = '/'> Перейти на страницу главная</a>
+      <p>Количество переходов = ${aboutCount++}</p>`);
   } else {
     res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
-    res.end(`<h1>Страница 404<h1/>
-      <p>Количество переходов = ${errorCount++}<p/>`);
+    res.end(`<h1>Страница 404</h1>
+      <p>Количество переходов = ${errorCount++}</p>`);
   }
 });
 
